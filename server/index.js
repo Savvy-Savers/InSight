d81ceca03811ad2d812a214433d9a7bac5298e77
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-25 15:04:37 -0600

    Rename module to course to avoid keyword confusion

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 const port = 8080;
 
 const profileRoutes = require('./routes/profile');
-const moduleRoutes = require('./routes/module');
+const moduleRoutes = require('./routes/course');
 const toolRoutes = require('./routes/tool');
 
 app.use(bodyParser.json());
 
 // Routes for the app
 app.use('/profile', profileRoutes);
-app.use('/module', moduleRoutes);
+app.use('/course', moduleRoutes);
 app.use('/tool', toolRoutes);
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
