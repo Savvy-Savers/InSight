ac63a283fe35e426d9ba5d48f4ae29a8070332fe
Amber <amber.j.nola@gmail.com>
2019-11-25 15:11:52 -0600

    fixed Main.js to Main.jsx

--- app/App.js
+++ app/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Main from './src/views/Main.js'
+import Main from './src/views/Main.jsx'
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
