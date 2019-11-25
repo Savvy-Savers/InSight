c5b3c38b898fde923be473d25e9ccd307dcffb05
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-25 15:32:24 -0600

    Create enpoints to retrieve courses

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
+const { getCourse, getCourses } = require('../db/helper');
 
-router.get('/', (req, res) => {
-  res.send('Hello World!');
+// Endpoint to retrieve a single course from id
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+  getCourse(id) //TODO: create getCourse helper for DB
+    .then((course) => {
+      res.json(course);
+    });
 });
 
+// Endpoint to retrieve all courses
+router.get('/list', (req, res) => {
+  getCourses() //TODO: create getCourses helper for DB
+    .then((courses) => {
+      res.json(courses);
+    });
+})
+;
 module.exports = router;
