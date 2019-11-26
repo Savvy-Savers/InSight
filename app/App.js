aa2a3aa5f3be025141a0a598d861cf7b5971096d
Amber <amber.j.nola@gmail.com>
2019-11-25 19:46:41 -0600

    saving successful views

--- app/App.js
+++ app/App.js
@@ -3,20 +3,23 @@ import Main from './src/views/Main.jsx'
 import { StyleSheet, Text, View } from 'react-native';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-
+import { createDrawerNavigator } from 'react-navigation-drawer';
 import CourseScreen from './src/views/Course';
 import MapScreen from './src/views/Map.jsx';
 import ProfileScreen from './src/views/Profile.jsx';
 import MainScreen from './src/views/Main.jsx';
 
 
+// This is the entry point for Expo! We cann't move this file - so instead, 
+// we are using it to return the rest of our app views. 
 export default class App extends React.Component {
   render() {
     return <AppContainer />;
   }
 }
 
-const AppNavigator = createStackNavigator(
+// This is our navigation router, these are the pages names, and the screens. 
+const AppNavigator = createDrawerNavigator(
   {
     Main: MainScreen,
     Course: CourseScreen,
