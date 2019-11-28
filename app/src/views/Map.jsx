981492fe85d0646f0b61bc3fa5c00ccb84db409d
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-28 12:34:04 -0600

    Add navigation to individual course page

--- app/src/views/Map.jsx
+++ app/src/views/Map.jsx
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import { ListItem, Header } from 'react-native-elements';
+import { useNavigation } from 'react-navigation-hooks';
 import axios from 'axios';
 
 
 function MapScreen() {
   const [courses, setCourses] = useState([]);
+  const { navigate } = useNavigation();
 
   useEffect(() => {
     axios.get('http://localhost:8080/course/list') // FIXME: modify for deployment
@@ -19,21 +21,19 @@ function MapScreen() {
       <Header // Temporary header for display purposes
         leftComponent={{ icon: 'menu', color: '#fff' }}
         centerComponent={{ text: 'Courses', style: { color: '#fff' } }}
-        rightComponent={{ icon: 'home', color: '#fff' }}
       />
       <View>
         {
-          courses.map((course) => {
-            return (
-              <ListItem
-                key={course.topic}
-                title={course.topic}
-                leftAvatar={{ source: { uri: 'https://i.chzbgr.com/full/8762786048/hDA3B4D87/' } }}
-                bottomDivider
-                chevron
-              />
-            );
-          })
+          courses.map((course) => (
+            <ListItem
+              key={course.topic}
+              title={course.topic}
+              leftAvatar={{ source: { uri: 'https://i.chzbgr.com/full/8762786048/hDA3B4D87/' } }}
+              bottomDivider
+              chevron
+              onPress={() => { navigate('Course', { id: course.id }); }}
+            />
+          ))
         }
       </View>
     </View>
