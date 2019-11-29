9209df9579ed0a59b8337e17e2599b5087ba2e02
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-29 17:21:18 -0600

    Prepare display to take in badges

--- app/src/views/Profile.jsx
+++ app/src/views/Profile.jsx
@@ -1,22 +1,43 @@
 import React, { useState, useEffect } from 'react';
 import { Text, View } from 'react-native';
+import { ListItem, Header } from 'react-native-elements';
 import axios from 'axios';
 
 function ProfileScreen() {
   // Profile info
   const [profile, setProfile] = useState({});
+  const [Badges, setBadges] = useState([]);
 
   useEffect(() => {
-    axios.get('http://localhost/profile/user/1') // FIXME: modify user id# for authentication
+    axios.get('http://18.206.35.110:8080/profile/user/1') // FIXME: modify user id# for authentication
       .then((profileData) => {
         setProfile(profileData.data);
       });
   }, []); // Array necessary to not repeat endlessly
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
-      <Text>{`${profile.totalExpriencePoints} XP`}</Text>
+    // Basic display to show necessary variables, to be revised
+    <View style={{ flex: 1, alignItems: 'center' }}>
+      <Header />
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
+        <Text>{`${profile.totalExpriencePoints} XP`}</Text>
+        <Text>Display Level Here</Text>
+        <Text>{`Goals: ${profile.goal}`}</Text>
+      </View>
+      <View style={{ flex: 5, alignItems: 'center' }}>
+        <Text>Badges go Here!</Text>
+        { // Map to display the badges
+          Badges.map((badge) => (
+            <ListItem
+              key={badge.name}
+              title={badge.name}
+              bottomDivider
+              subtitle={badge.description}
+            />
+          ))
+        }
+      </View>
     </View>
   );
 }
