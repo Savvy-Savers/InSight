25633583c51a8fd38f95acc087a4339b3b86617d
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-29 16:44:59 -0600

    Modify profile to use hooks and display first username and experience

--- app/src/views/Profile.jsx
+++ app/src/views/Profile.jsx
@@ -1,12 +1,24 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View } from 'react-native';
+import axios from 'axios';
 
-export default class ProfileScreen extends React.Component {
-    render() {
-        return (
-            <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-                <Text>Profile Screen</Text>
-            </View>
-        );
-    }
+function ProfileScreen() {
+  // Profile info
+  const [profile, setProfile] = useState({});
+
+  useEffect(() => {
+    axios.get('http://localhost/profile/user/1') // FIXME: modify user id# for authentication
+      .then((profileData) => {
+        setProfile(profileData.data);
+      });
+  }, []); // Array necessary to not repeat endlessly
+
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
+      <Text>{`${profile.totalExpriencePoints} XP`}</Text>
+    </View>
+  );
 }
+
+export default ProfileScreen;
