dda33c3dcb8970d45ee434ac562d2d8d7552fb7b
Eliott Frilet <frilet.eliott@gmail.com>
2019-11-30 14:28:49 -0600

    Retrieve and display acquired badges from database

--- app/src/views/Profile.jsx
+++ app/src/views/Profile.jsx
@@ -6,32 +6,37 @@ import axios from 'axios';
 function ProfileScreen() {
   // Profile info
   const [profile, setProfile] = useState({});
-  const [Badges, setBadges] = useState([]);
+  const [badges, setBadges] = useState([]);
 
   useEffect(() => {
     axios.get('http://18.206.35.110:8080/profile/user/1') // FIXME: modify user id# for authentication
       .then((profileData) => {
         setProfile(profileData.data);
+        return axios.get('http://18.206.35.110:8080/profile/user/1/badges');
+      })
+      .then((badgesData) => {
+        setBadges(badgesData.data);
       });
   }, []); // Array necessary to not repeat endlessly
 
   return (
     // Basic display to show necessary variables, to be revised
-    <View style={{ flex: 1, alignItems: 'center' }}>
+    <View style={{ flex: 1 }}>
       <Header />
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Text>{`${profile.firstName} ${profile.lastName}`}</Text>
         <Text>{`${profile.totalExpriencePoints} XP`}</Text>
-        <Text>Display Level Here</Text>
+        <Text>Acquired Badges!</Text>
         <Text>{`Goals: ${profile.goal}`}</Text>
       </View>
-      <View style={{ flex: 5, alignItems: 'center' }}>
+      <View style={{ flex: 5 }}>
         <Text>Badges go Here!</Text>
         { // Map to display the badges
-          Badges.map((badge) => (
+          badges.map((badge) => (
             <ListItem
               key={badge.name}
               title={badge.name}
+              leftAvatar={{ source: { uri: badge.iconUrl } }}
               bottomDivider
               subtitle={badge.description}
             />
