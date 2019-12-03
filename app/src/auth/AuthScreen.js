e88266e9b15c79547eddb321da96904e2b0b12be
Titus Charles <tcharles23.tc@gmail.com>
2019-12-03 11:31:47 -0600

    add icon to button

--- app/src/auth/AuthScreen.js
+++ app/src/auth/AuthScreen.js
@@ -2,22 +2,29 @@ import React from 'react';
 import {
   Button,
   View,
-  StyleSheet
+  StyleSheet,
+  Icon,
 } from 'react-native';
 import { Auth } from 'aws-amplify';
 
 class AuthScreen extends React.Component {
   render() {
     return (
-      <View style={styles.authScreen} >
+      <View style={styles.authScreen}>
         <Button
           onPress={() => Auth.federatedSignIn({ provider: 'google' })}
-          title="Signin with Google"
+          title="Signin"
+          icon={
+            <Icon
+              name="arrow-right"
+              size={15}
+              color="white"
+            />
+          }
         />
       </View>
     );
   }
-
 }
 
 export default AuthScreen;
@@ -26,6 +33,6 @@ const styles = StyleSheet.create({
   authScreen: {
     flex: 1,
     justifyContent: 'center',
-    alignItems: 'center'
-  }
+    alignItems: 'center',
+  },
 });
