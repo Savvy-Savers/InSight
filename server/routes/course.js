c08a817b5bf3c5cfd7f63e249b8200719fdf1bba
Amber <amber.j.nola@gmail.com>
2019-12-04 18:01:56 -0600

    have helper and route in place

--- server/routes/course.js
+++ server/routes/course.js
@@ -1,5 +1,11 @@
 const router = require('express').Router();
-const { getCourse, getCourses } = require('../db/helper');
+const {
+  getCourse,
+  getCourses,
+  getCourseBadge,
+  updateUserXp,
+  insertUserBadge,
+} = require('../db/helper');
 
 // Endpoint to retrieve a single course from id
 router.get('/list/:id', (req, res) => {
@@ -18,6 +24,28 @@ router.get('/list', (req, res) => {
   getCourses()
     .then((courses) => {
       res.json(courses);
+    })
+    .catch((err) => {
+      console.error(err);
+    });
+});
+
+// Endpoint to retrieve the badge for the course
+router.get('/badge/:badgeId', (req, res) => {
+  const { badgeId } = req.params;
+  getCourseBadge(badgeId).then((badge) => {
+    res.json(badge);
+  })
+    .catch((err) => console.error(err));
+});
+
+// Endpoint to create user badge connection when they complete a course
+router.post('/user/:id/badge/:badgeId', (req, res) => {
+  const { id, badgeId } = req.params;
+  updateUserXp(id, badgeId)
+    .then(() => insertUserBadge(id, badgeId))
+    .then(() => {
+      res.sendStatus(201);
     })
     .catch((err) => {
       console.error(err);
