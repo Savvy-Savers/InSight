bb104cb8733e7c698fa8abe59b088646441a81d7
Titus Charles <tcharles23.tc@gmail.com>
2019-12-05 19:46:20 -0600

    add givenName, familyName, and email to the user model

--- server/db/model/user.js
+++ server/db/model/user.js
@@ -2,11 +2,10 @@ module.exports = (sequelize, type) => sequelize.define('user', {
   id: {
     type: type.INTEGER,
     primaryKey: true,
-    autoIncrement: true,
   },
-  firstName: type.STRING,
-  lastName: type.STRING,
-  email: type.String,
+  givenName: type.STRING,
+  familyName: type.STRING,
+  email: type.STRING,
   totalExperiencePoints: type.INTEGER,
   goal: type.STRING,
 });
