6ac23594c98aa7be80ffeb083867d54b1e97a206
Titus Charles <tcharles23.tc@gmail.com>
2019-12-06 10:50:54 -0600

    changed the firstName and LastName to givenName and familyName to match google. set google id to a string

--- server/db/model/user.js
+++ server/db/model/user.js
@@ -2,10 +2,12 @@ module.exports = (sequelize, type) => sequelize.define('user', {
   id: {
     type: type.INTEGER,
     primaryKey: true,
+    autoIncrement: true,
   },
   givenName: type.STRING,
   familyName: type.STRING,
   email: type.STRING,
+  googleId: type.STRING,
   totalExperiencePoints: type.INTEGER,
   goal: type.STRING,
 });
