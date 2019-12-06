808af07e2a809ad631d80607b7e858ab86d6e6d6
Eliott Frilet <frilet.eliott@gmail.com>
2019-12-06 10:25:01 -0600

    Creat endpoint to update user's budget

--- server/routes/tool.js
+++ server/routes/tool.js
@@ -1,5 +1,10 @@
 const router = require('express').Router();
-const { getBudget, setBudget, spendBudget } = require('../db/helper');
+const {
+  getBudget,
+  setBudget,
+  spendBudget,
+  updateBudget,
+} = require('../db/helper');
 
 // Endpoint to get user's saved budget info by id
 router.get('/budget/:id', (req, res) => {
@@ -21,13 +26,23 @@ router.post('/budget/:id', (req, res) => {
     .catch((err) => console.error(err));
 });
 
-// Endpoint to update user's spent amount in their budget
+// Endpoint to update user's budget
 router.patch('/budget/:id', (req, res) => {
+  const { id } = req.params;
+  updateBudget(id, req.body)
+    .then(() => {
+      res.sendStatus(204);
+    })
+    .catch((err) => console.error(err));
+});
+
+// Endpoint to update user's spent amount in their budget
+router.patch('/budget/spend/:id', (req, res) => {
   const { id } = req.params;
   const { spend } = req.body;
   spendBudget(id, spend)
     .then(() => {
-      res.sendStatus(200);
+      res.sendStatus(204);
     })
     .catch((err) => console.error(err));
 });
