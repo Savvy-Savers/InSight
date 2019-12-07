0089db396be28766c470cddb8b6a6fe4b0ad5ea2
Eliott Frilet <frilet.eliott@gmail.com>
2019-12-06 18:40:58 -0600

    Replace temporary header with NavBar component

--- app/src/views/Main.jsx
+++ app/src/views/Main.jsx
@@ -3,11 +3,9 @@ import {
   StyleSheet,
   Text,
   View,
-  TouchableOpacity,
   ImageBackground,
 } from 'react-native';
-import { Header } from 'react-native-elements';
-import Icon from 'react-native-vector-icons/FontAwesome';
+import NavBar from './NavBar';
 
 const styles = StyleSheet.create({
   container: {
@@ -18,27 +16,11 @@ const styles = StyleSheet.create({
   },
 });
 
-function MainScreen() {
+function MainScreen(props) {
   return (
     <View style={{ flex: 1 }}>
       <ImageBackground source={{ uri: './src/assets/images/cascade.png' }} style={{ width: '100%', height: '100%' }}>
-        <Header // Temporary header with button to eventually open drawer
-          leftComponent={
-            (
-              <TouchableOpacity onPress={() => { /* Open Drawer */ }}>
-                <Icon
-                  name="bars"
-                  style={{
-                    color: 'white',
-                    padding: 10,
-                    marginLeft: 10,
-                    fontSize: 20,
-                  }}
-                />
-              </TouchableOpacity>
-            )
-          }
-        />
+        <NavBar navigation={props.navigation} />
         <View style={styles.container}>
           <Text>Welcome to InSight!</Text>
         </View>
