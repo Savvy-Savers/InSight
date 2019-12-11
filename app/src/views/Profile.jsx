fc3ccf62020369f7dd95d93bb982f0342f8c31b8
Eliott Frilet <frilet.eliott@gmail.com>
2019-12-11 12:10:02 -0600

    Add level data retrieval and XP bar

--- app/src/views/Profile.jsx
+++ app/src/views/Profile.jsx
@@ -21,6 +21,14 @@ const styles = StyleSheet.create({
     borderWidth: 3,
     borderRadius: 150,
   },
+  progressBar: {
+    height: 20,
+    width: '100%',
+    backgroundColor: 'white',
+    borderColor: '#000',
+    borderWidth: 2,
+    borderRadius: 5,
+  },
 });
 
 class ProfileScreen extends React.Component {
@@ -29,6 +37,8 @@ class ProfileScreen extends React.Component {
     this.state = {
       profile: {},
       badges: [],
+      currentLevel: {},
+      nextLevel: {},
     };
 
     this.getData = this.getData.bind(this);
@@ -55,14 +65,28 @@ class ProfileScreen extends React.Component {
         return axios.get(`http://${deployment}:8080/profile/user/${profileData.data.id}/badges`);
       })
       .then((badgesData) => {
+        const { profile } = this.state;
         this.setState({
           badges: badgesData.data,
+        });
+        return axios.get(`http://${deployment}:8080/profile/user/${profile.id}/level`);
+      })
+      .then((levelData) => {
+        console.log(levelData.data);
+        this.setState({
+          currentLevel: levelData.data.currentLevel,
+          nextLevel: levelData.data.nextLevel,
         });
       });
   }
 
   render() {
-    const { profile, badges } = this.state;
+    const {
+      profile,
+      badges,
+      currentLevel,
+      nextLevel,
+    } = this.state;
     return (
       // Basic display to show necessary variables, to be revised
       <View style={{ flex: 1 }}>
@@ -70,7 +94,16 @@ class ProfileScreen extends React.Component {
         <View style={{ alignItems: 'center', justifyContent: 'center', margin: 5 }}>
           <Image style={styles.image} source={{ uri: profile.photoUrl }} />
           <Text>{`${profile.givenName} ${profile.familyName}`}</Text>
+          <Text>{`Level: ${currentLevel.name}`}</Text>
           <Text>{`${profile.totalExperiencePoints} XP`}</Text>
+          <View style={styles.progressBar}>
+            <View style={{
+              ...StyleSheet.absoluteFill,
+              backgroundColor: '#8BED4F',
+              width: `${(nextLevel.experiencePointsThreshold - currentLevel.experiencePointsThreshold) / profile.totalExperiencePoints}%`,
+            }}
+            />
+          </View>
           {/* <Text>{`Goals: ${profile.goal}`}</Text> */}
         </View>
         <View style={{ flex: 5, margin: 5 }}>
